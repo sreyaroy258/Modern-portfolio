0c9f4de4ff591cd6efd88e485d2b79768be71d3b
sreyaroy258 <158825510+sreyaroy258@users.noreply.github.com>
2025-05-22 01:30:05 +0530

    14th fix

--- src/components/contact/ContactMeRight.jsx
+++ src/components/contact/ContactMeRight.jsx
@@ -4,11 +4,11 @@ import ContactSocial from "./ContactSocial";
 const ContactMeRight = () => {
   return (
     <div className="flex flex-col items-center justify-center gap-12">
-      <img
+      {/* <img
   src="/images/email-image.png"
   alt="test"
   className="max-w-[300px]"
-/>
+/> */}
 
       <ContactInfo />
       <ContactSocial />
