ae5d37f37ae0bdeb896280431d7581c97c7fff47
sreyaroy258 <158825510+sreyaroy258@users.noreply.github.com>
2025-05-22 01:16:41 +0530

    6th

--- src/components/experience/ExperienceTopMiddle.jsx
+++ src/components/experience/ExperienceTopMiddle.jsx
@@ -1,7 +1,11 @@
 const ExperienceTopMiddle = () => {
   return (
     <div className="lg:w-[35%] md:w-[50%] sm:w-[80%]">
-      <img src="/images/experience-image.png" alt="my experience" />
+      <img
+        src="/images/experience-image.png"
+        alt="Illustration representing my work experience"
+        className="w-full h-auto"
+      />
     </div>
   );
 };
