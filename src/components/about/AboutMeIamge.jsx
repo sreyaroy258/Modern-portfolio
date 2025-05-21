45e2c6ef30b057b5e0af9249001d995ba5c6196e
sreyaroy258 <158825510+sreyaroy258@users.noreply.github.com>
2025-05-22 01:17:48 +0530

    7th

--- src/components/about/AboutMeIamge.jsx
+++ src/components/about/AboutMeIamge.jsx
@@ -3,9 +3,9 @@ const AboutMeImage = () => {
     <div className="h-[500px] w-[300px] relative">
       <div className="h-[500px] w-[300px] rounded-[100px] absolute overflow-hidden">
         <img
-          src="../../public/images/about-me.jpg"
+          src="/images/about-me.jpg"  // <-- fixed path here
           alt="About Me Image"
-          className="h-full w-auto object-cover"
+          className="h-full w-full object-cover"
         />
       </div>
       <div className="h-[500px] w-[250px] bg-orange absolute bottom-[-30px] left-[-30px] rounded-bl-[120px] rounded-tr-[120px] rounded-br-[20px] rounded-tl-[20px] -z-10"></div>
